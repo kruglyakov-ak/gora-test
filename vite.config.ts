d19dd03d8965f766b68cd9793e668d3f9cea2401
Aleksandr Kruglyakov <kruglyakova@onlinepatent.ru>
2026-04-04 16:12:59 +0300

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    babel({
+      babelConfig: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
