9ff9f2d3cb34a7f6175e2c3391dc75fdf847b530
Aleksandr Kruglyakov <kruglyakova@onlinepatent.ru>
2026-04-05 23:43:37 +0300

    feat: add styles

--- src/features/chat/ui/Chat.tsx
+++ src/features/chat/ui/Chat.tsx
@@ -5,14 +5,12 @@ import { MessagesList } from './MessagesList';
 export function Chat() {
   return (
     <div className="h-full">
-      <div className="flex flex-col mx-auto h-full border-0 border-base-300 md:rounded-2xl overflow-hidden md:border md:container">
+      <div className="flex flex-col h-dvh md:h-full mx-auto border-0 border-base-300 md:rounded-2xl overflow-hidden md:border md:container chat-container">
         <ChatHeader
           name="ChatBot"
           avatar="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
         />
-
         <MessagesList />
-
         <ChatTextArea />
       </div>
     </div>
--- src/pages/chat/ChatPage.tsx
+++ src/pages/chat/ChatPage.tsx
@@ -2,7 +2,7 @@ import { Chat } from '@/features/chat';
 
 export function ChatPage() {
   return (
-    <div className="w-screen h-screen overflow-hidden md:py-4">
+    <div className="w-screen h-dvh overflow-hidden md:py-4">
       <Chat />
     </div>
   );
